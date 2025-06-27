3cfba27fb428c5cb5881abc4740c084a9607a873
prequena-84 <pedrorequenarondon@hotmail.com>
2025-06-26 22:20:47 -0400

    feacture-dev-estructure-and-types-2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './config/database/database.module';
 
 @Module({
-  //imports: [],
-  controllers: [
-    AppController,
+  imports: [
+    DatabaseModule,
   ],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {};
+
+// Quede en crear la carpetas de los tipos e interfaces y averiguar sobre las bibliotecas de javascript.
